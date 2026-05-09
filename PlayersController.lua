235b1b5e9473ee85298d7a299692004956b450f9
jaheerman3312301105 <frdhn2323@gmail.com>
2026-05-09 15:03:33 +0700

    commit

--- PlayersController.lua
+++ PlayersController.lua
@@ -1,13 +1,14 @@
 local PlayersController = {}
-local humanoid = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
-
 
 function PlayersController:SetWalkSpeed(speed)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speed
 end
 
 function PlayersController:SetJumpPower(power)
+    local humanoid = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
+
     humanoid.UseJumpPower = true
     humanoid.JumpPower = power
 end
+
 return PlayersController
