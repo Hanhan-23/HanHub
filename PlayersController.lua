b294d89d09444962488a15496c8199da0a2f950b
jaheerman3312301105 <frdhn2323@gmail.com>
2026-05-09 15:02:06 +0700

    test

--- PlayersController.lua
+++ PlayersController.lua
@@ -1,11 +1,13 @@
 local PlayersController = {}
+local humanoid = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
+
 
 function PlayersController:SetWalkSpeed(speed)
     game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = speed
 end
 
 function PlayersController:SetJumpPower(power)
-    game.Players.LocalPlayer.Character.Humanoid.JumpPower = power
+    humanoid.UseJumpPower = true
+    humanoid.JumpPower = power
 end
-
 return PlayersController
